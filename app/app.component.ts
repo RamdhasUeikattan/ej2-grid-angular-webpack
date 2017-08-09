496d7d956dfdcb16105af9a2d9fecd0213bcf94b
ROOT\ramdhas.ueikattan <ramdhas.ueikattan@syncfusion.com>
2017-08-09 16:22:04 +0530

    Syncfusion Group By Aggreation sample

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,22 +1,54 @@
 import { Component, OnInit  } from '@angular/core';
-import { data } from './data';
+import { data  } from './data';
 @Component({
     selector: 'my-app',
-    template: `<ej-grid [dataSource]='data' height="315px" allowPaging="true">
-                <e-columns>
-                    <e-column field='OrderID' headerText='Order ID' textAlign='right' width=90></e-column>
-                    <e-column field='CustomerID' headerText='Customer ID' width=120></e-column>
-                    <e-column field='Freight' headerText='Freight' textAlign='right' format='C2' width=90></e-column>
-                    <e-column field='OrderDate' headerText='Order Date' textAlign='right' format='yMd' width=120></e-column>
-                </e-columns>
+    template: `<ej-grid #grid [dataSource]='data' [allowPaging]='true' [toolbar]='toolbar' [allowFiltering] = 'true' [allowSorting] = 'true' [allowGrouping] = 'true'>
+                    <e-columns>
+                        <e-column field='OrderID' headerText='Order ID' width='120' textAlign="right"></e-column>
+                        <e-column field='ShipCity' headerText='Ship City' width='140'></e-column>
+                        <e-column field='Freight' headerText='Freight' width='170' format= 'C2'></e-column>
+                        <e-column field='ShipName' headerText='ShipName' width='120' ></e-column>
+                    </e-columns>
+                    <e-aggregates>
+                        <e-aggregate>
+                            <e-columns> 
+                                <e-column type="sum" field="Freight" format="C2">
+                                    <template #footerTemplate let-data>Sum: {{data.sum}}</template>
+                                </e-column>
+                            </e-columns>
+                        </e-aggregate>
+                        <e-aggregate>
+                            <e-columns>
+                                <e-column type="min" field="Freight" format="C2">
+                                    <template #groupFooterTemplate let-data>min: {{data.min}}</template>
+                                </e-column>
+                            </e-columns>
+                        </e-aggregate>
+                        <e-aggregate>
+                            <e-columns>
+                                <e-column type="max" field="Freight" format="C2">
+                                    <template #groupFooterTemplate let-data>max: {{data.max}}</template>
+                                </e-column>
+                            </e-columns>
+                        </e-aggregate>
+                        <e-aggregate>
+                        <e-columns>
+                            <e-column type="sum" field="Freight"  format="C2">
+                                <template #groupFooterTemplate let-data>Group Sum: {{data.sum}}</template>
+                            </e-column>
+                        </e-columns>
+                    </e-aggregate>
+                    </e-aggregates> 
                 </ej-grid>`
 })
 export class AppComponent implements OnInit {
 
     public data: Object[];
+    public toolbar: string[];
 
     ngOnInit(): void {
         this.data = data;
+        this.toolbar = ['search'];
     }
 
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { GridModule, PageService } from '@syncfusion/ej2-ng-grids';
+import { GridModule, PageService, FilterService,  ToolbarService, AggregateService, GroupService, SortService } from '@syncfusion/ej2-ng-grids';
 import { AppComponent }  from './app.component';
 
 @NgModule({
   imports:      [ BrowserModule, GridModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ],
-  providers: [PageService]
+  providers: [PageService, FilterService,ToolbarService, AggregateService, GroupService, SortService]
 })
 export class AppModule { }
